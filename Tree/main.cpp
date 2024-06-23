568d856df203f40adc01658c6aa324b8831fc31c
hungryBeetle <erik.ts@outlook.com>
2024-06-23 14:41:14 -0300

    Rename Node addNode function. Create Tree addNode function.

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 class Node{
 	public:
 	Node(int value): nodeValue{value}, left{nullptr}, right{nullptr}{}
@@ -5,14 +7,14 @@ class Node{
 	Node* left;
 	Node* right;
 	
-	void addNode(int nodeValue){
+	void addChildNode(int nodeValue){
 		if(nodeValue < this->nodeValue){
 			if(this->left == nullptr){ this->left = new Node(nodeValue); }
-			else{ this->left->addNode(nodeValue); }
+			else{ this->left->addChildNode(nodeValue); }
 		}
 		else{
 			if(this->right == nullptr){ this->right = new Node(nodeValue); }
-			else{ this->right->addNode(nodeValue); }
+			else{ this->right->addChildNode(nodeValue); }
 		}
 	}
 };
@@ -21,6 +23,12 @@ class Tree{
 	Tree(int nodeValue): root{new Node(nodeValue)}{}
 	public:
 	Node* root;
+
+	void addNode(int newValue){
+		if(newValue < this->root->nodeValue) { this->root->left->addChildNode(newValue); }
+		else if(newValue > this->root->nodeValue) { this->root->right->addChildNode(newValue); }
+		else{ std::cout << newValue << " has already been added."; }
+	}
 };
 
 int main(){
