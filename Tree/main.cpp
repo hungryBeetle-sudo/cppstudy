6613adaadfaac3450eadbcf8a41a346d96abc2fc
hungryBeetle <erik.ts@outlook.com>
2024-06-26 21:46:54 -0300

    Fix Tree addNode error. Create Node removeChildNode function.

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -20,6 +20,28 @@ class Node{
 		}
 		else{ std::cout << nodeValue << " has already been added."; }
 	}
+
+	void removeChildNode(int nodeValue){
+		int diff = nodeValue - this->nodeValue;
+
+		if(diff < 0){
+			if(this->left == nullptr){ std::cout << nodeValue << " hasn't been found."; }
+			else if(this->left->nodeValue == nodeValue){ 
+				delete this->left;
+				this->left = nullptr;
+			}
+			else{ this->left->removeChildNode(nodeValue); }
+		}
+		else if(diff > 0){	
+			if(this->right == nullptr){ std::cout << nodeValue << " hasn't been found."; }
+			else if(this->right->nodeValue == nodeValue){ 
+				delete this->right;
+				this->right = nullptr;
+			}
+			else{ this->right->removeChildNode(nodeValue); }
+		}
+		
+	}
 };
 
 class Tree{
@@ -31,6 +53,15 @@ class Tree{
 		if(newValue < this->root->nodeValue) { this->root->left->addChildNode(newValue); }
 		else if(newValue > this->root->nodeValue) { this->root->right->addChildNode(newValue); }
 		else{ std::cout << newValue << " has already been added."; }
+	}
+
+	void removeNode(int removedValue){
+		if(removedValue < this->root->nodeValue){ this->root->left->removeChildNode(removedValue); }
+		else if(removedValue > this->root->nodeValue){ this->root->right->removeChildNode(removedValue); }
+		else{ 
+			delete this->root
+			this->root = nullptr;
+		}
 	}
 };
 
