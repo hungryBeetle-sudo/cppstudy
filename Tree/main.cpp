0ef331277a38dbba18063c40dc652878c47c1565
hungryBeetle <erik.ts@outlook.com>
2024-07-03 07:57:10 -0300

    Create searchNode function.

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -6,15 +6,15 @@ class Node{
 	int nodeValue;
 	Node* left;
 	Node* right;
-	
+
 	void addChildNode(int nodeValue){
 		int diff = nodeValue - this->nodeValue;
 
-		if(diff < 0){ 
+		if(diff < 0){
 			if(this->left == nullptr){ this->left = new Node(nodeValue); }
 			else{ this->left->addChildNode(nodeValue); }
 		}
-		else if(diff > 0){ 
+		else if(diff > 0){
 			if(this->right == nullptr){ this->right = new Node(nodeValue); }
 			else{ this->right->addChildNode(nodeValue); }
 		}
@@ -26,21 +26,21 @@ class Node{
 
 		if(diff < 0){
 			if(this->left == nullptr){ std::cout << nodeValue << " hasn't been found."; }
-			else if(this->left->nodeValue == nodeValue){ 
+			else if(this->left->nodeValue == nodeValue){
 				delete this->left;
 				this->left = nullptr;
 			}
 			else{ this->left->removeChildNode(nodeValue); }
 		}
-		else if(diff > 0){	
+		else if(diff > 0){
 			if(this->right == nullptr){ std::cout << nodeValue << " hasn't been found."; }
-			else if(this->right->nodeValue == nodeValue){ 
+			else if(this->right->nodeValue == nodeValue){
 				delete this->right;
 				this->right = nullptr;
 			}
 			else{ this->right->removeChildNode(nodeValue); }
 		}
-		
+
 	}
 };
 
@@ -48,6 +48,29 @@ class Tree{
 	public:
 	Tree(int nodeValue): root{new Node(nodeValue)}{}
 	Node* root;
+
+	Node* searchNode(int target){
+		Node* current = this->root;
+		bool finishSearch = false;
+
+		while(!finishSearch){
+			if(current->nodeValue == target){
+				finishSearch = true;
+			}
+			else if(current->nodeValue < target && current->left != nullptr){
+				current = current->left;
+			}
+			else if(current->nodeValue > target && current->right != nullptr){
+				current = current->right;
+			}
+			else{
+				finishSearch = true;
+				current = nullptr;
+			}
+		}
+		
+		return current;
+	}
 
 	void addNode(int newValue){
 		if(newValue < this->root->nodeValue) { this->root->left->addChildNode(newValue); }
@@ -58,8 +81,8 @@ class Tree{
 	void removeNode(int removedValue){
 		if(removedValue < this->root->nodeValue){ this->root->left->removeChildNode(removedValue); }
 		else if(removedValue > this->root->nodeValue){ this->root->right->removeChildNode(removedValue); }
-		else{ 
-			delete this->root
+		else{
+			delete this->root;
 			this->root = nullptr;
 		}
 	}
